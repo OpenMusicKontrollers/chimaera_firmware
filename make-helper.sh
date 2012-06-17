197a72614ded9e381b8939d877cbeebe83399c3f
Hanspeter Portner <agenthp@users.sf.net>
2012-06-17 11:22:13 +0200

    added default build env variables

--- make-helper.sh
+++ make-helper.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-LIB_MAPLE_HOME=$HOME/src/libmaple
-BOARD=maple_mini
+export LIB_MAPLE_HOME=$HOME/src/libmaple
+export BOARD=maple_mini
 
 SCRIPT=`readlink -f $0`
 USER_MODULES=`dirname $SCRIPT` make -f $LIB_MAPLE_HOME/Makefile $@
